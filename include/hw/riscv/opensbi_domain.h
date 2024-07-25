b9ecf605bd5e6c5370011790355c8aa1e536afb9
Gregor Haas <gregorhaas1997@gmail.com>
2024-07-25 16:21:12 -0700

    Add support for generating OpenSBI domains in the device tree
    OpenSBI has support for domains, which are partitions of CPUs and memory into
    isolated compartments. Domains can be specified in the device tree according to
    a standardized format [1], which OpenSBI parses at boot time to initialize all
    system domains. This patch enables simply specifying domains (and their
    associated memory regions) on the QEMU command line, from which these are then
    rendered into the machine's device tree.
    
    At machine initialization time, a new create_fdt_opensbi_domains() function
    walks the peripherals/peripherals-anon containers, identifies all domains and
    memregions, and parses them into the relevant device tree structures.
    
    [1] https://github.com/riscv-software-src/opensbi/blob/master/docs/domain_support.md
    
    Signed-off-by: Gregor Haas <gregorhaas1997@gmail.com>

---
+++ include/hw/riscv/opensbi_domain.h
@@ -0,0 +1,50 @@
+
+#ifndef RISCV_DOMAIN_H
+#define RISCV_DOMAIN_H
+
+#include "hw/sysbus.h"
+#include "qom/object.h"
+#include "cpu.h"
+
+#define TYPE_OPENSBI_MEMREGION "opensbi-memregion"
+OBJECT_DECLARE_SIMPLE_TYPE(OpenSBIMemregionState, OPENSBI_MEMREGION)
+
+#define OPENSBI_MEMREGION_DEVICES_MAX   16
+
+struct OpenSBIMemregionState {
+    /* public */
+    DeviceState parent_obj;
+
+    /* private */
+    uint64_t base;
+    uint32_t order;
+    bool mmio;
+    char *devices[OPENSBI_MEMREGION_DEVICES_MAX];
+};
+
+#define TYPE_OPENSBI_DOMAIN "opensbi-domain"
+OBJECT_DECLARE_SIMPLE_TYPE(OpenSBIDomainState, OPENSBI_DOMAIN)
+
+#define OPENSBI_DOMAIN_MEMREGIONS_MAX   16
+
+struct OpenSBIDomainState {
+    /* public */
+    DeviceState parent_obj;
+
+    /* private */
+    OpenSBIMemregionState *regions[OPENSBI_DOMAIN_MEMREGIONS_MAX];
+    unsigned int region_perms[OPENSBI_DOMAIN_MEMREGIONS_MAX];
+    unsigned long first_possible_hart, last_possible_hart;
+    unsigned int boot_hart;
+    uint64_t next_arg1;
+    uint64_t next_addr;
+    uint32_t next_mode;
+    bool system_reset_allowed;
+    bool system_suspend_allowed;
+
+    bool assign;
+};
+
+void create_fdt_opensbi_domains(MachineState *s);
+
+#endif /* RISCV_DOMAIN_H */
